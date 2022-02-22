2a32cd41d1e245f63c059385abc25c48fbe3da14
m_muto@jour-1.com <m_muto@jour-1.com>
2022-02-23 00:04:24 +0900

    added recyclerview

--- app/src/main/java/com/example/listshare/MainActivity.java
+++ app/src/main/java/com/example/listshare/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.listshare;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
@@ -10,5 +12,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RecyclerView recyclerView = findViewById(R.id.main_recycler_view);
+
+        //fix recyclerview layout size
+        recyclerView.setHasFixedSize(true);
+
+        //set recyclerview to layout manager
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager((layoutManager));
+
+        //make adapter and set to recyclerview
+        RecyclerView.Adapter mainAdapter = new MainAdapter(createRowData());
+        recyclerView.setAdapter(mainAdapter);
+    }
+
+    private String[] createRowData(){
+        String[] dataSet = new String[10];
+        int i = 0;
+        while(i < 10){
+            dataSet[i] = "hogehoge";
+            i++;
+        }
+        return dataSet;
     }
 }
